8e4b97f713d69835d417159988a460f1c9a5fac2
alex <lucillespinkamazon@gmail.com>
2024-05-23 09:24:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 
-Author: Aleksandr S.
+Author: Lazaurus S.
 Description: Revermse polish notation graphing calculator that has a very broken graphing situation but correctly working everything else.
 Date Last Modified: June 20th, 2023.
 
